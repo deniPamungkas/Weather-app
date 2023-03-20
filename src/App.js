6b6637cd0368277c050cf2a19c8f1a3c214b550f
deniPamungkas <pamungkasdeni18@gmail.com>
2023-03-20 22:02:18 +0700

    commit siap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import "./App.scss";
 import React, { useState } from "react";
 import axios from "axios";
-import sun from "./bc/sun.jpg"
-import rain from "./bc/rain.jpg"
-import cloud1 from "./bc/01cloud.jpg"
-import cloud2 from "./bc/02cloud.jpg"
+import sun from "./bc/sun.jpg";
+import rain from "./bc/rain.jpg";
+import cloud1 from "./bc/01cloud.jpg";
+import cloud2 from "./bc/02cloud.jpg";
 
 function App() {
-  const [datan, setDatan] = useState({})
+  const [datan, setDatan] = useState({});
   const [city, setCity] = useState("");
   const key = "b6d68ece3515ef2975e60deb754af423";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
   const getData = (event) => {
     if (event.key === "Enter") {
+      console.log("getdata");
       axios.get(url).then((response) => {
         const data = response.data;
         const result = {
-          kota : data.name,
-          suhu : Math.floor(data.main.temp)-273,
-          hum : data.main.humidity,
-          desc : data.weather[0].description,
-          feel : Math.floor(data.main.feels_like)-273,
-          wind : data.wind.speed
+          kota: data.name,
+          suhu: Math.floor(data.main.temp) - 273,
+          hum: data.main.humidity,
+          desc: data.weather[0].description,
+          feel: Math.floor(data.main.feels_like) - 273,
+          wind: data.wind.speed,
         }
         setDatan(result);
         setCity("");
@@ -29,25 +30,28 @@ function App() {
     }
   };
   const change = (e) => {
+    console.log("change");
     setCity(e.target.value);
   };
-  const getBackground = () => {
-    if(datan.desc === "scattered clouds" || datan.desc === "broken clouds"){
-      return cloud1
-    }else if(datan.desc === "overcast clouds"){
-      return cloud2
-    }else if(datan.desc === "rain"){
-      return rain
-    }else{
-      return sun
+  const getBackground = (e) => {
+    console.log("background");
+    if (e === "scattered clouds" || e === "broken clouds") {
+      return cloud1;
+    } else if (e === "overcast clouds") {
+      return cloud2;
+    } else if (e === "rain") {
+      return rain;
+    } else {
+      return sun;
     }
-  }
+  };
   return (
     <div className="App">
-    <img className="background" src={getBackground()} alt="background"/>
+      <img className="background" src={getBackground(datan.desc)} alt="background" />
       <div className="container">
         <div className="top">
-          <input value={city}
+          <input
+            value={city}
             onKeyPress={getData}
             onChange={change}
             className="input"
